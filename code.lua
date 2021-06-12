d9f1782cc325e472d24c4ee9e0ed40817f5a3f69
Robert Mah <rjmah1@ualberta.ca>
2021-06-11 23:22:04 -0600

    added pot and time shifting

--- code.lua
+++ code.lua
@@ -22,9 +22,22 @@ local player = {
     sprite = 1
 }
 
+local plantPot = {
+    position = {
+        x = 50.0,
+        y = 112.0,
+    },
+    pickedUp = false,
+    sprite = 5
+}
+
 local flags = {
     solid = 0,
 }
+
+local upHeldDown = false
+local bHeldDown = false
+local isFuture = false
 
 -- Based on https://www.lexaloffle.com/bbs/?tid=27626
 
@@ -35,6 +48,7 @@ end
 
 function Update(timeDelta)
     local startx = player.position.x
+    local starty = player.position.y
 
     if not player.onGround then
         player.acceleration = gameRules.acceleration / 2
@@ -44,8 +58,8 @@ function Update(timeDelta)
         player.decceleration = gameRules.decceleration
     end
 
-    -- Apply player jump velocity when they press up and are grounded
-    if (Button(Buttons.A, InputState.Down) or Button(Buttons.B, InputState.Down) or Button(Buttons.Up, InputState.Down)) and player.onGround then
+    -- Apply player jump velocity when they press A and are grounded
+    if Button(Buttons.A, InputState.Down) and player.onGround then
         player.velocity.y = -player.jumpPower
     end
 
@@ -76,24 +90,30 @@ function Update(timeDelta)
         player.velocity.x = -player.velocity.max
     end
 
+    -- Plant pot collision x
+    if math.abs(player.position.x + player.velocity.x - plantPot.position.x) < 8 and math.abs(player.position.y - plantPot.position.y) == 0 then
+        player.position.x = startx
+        player.velocity.x = 0
+    end
+    
     -- Move the player
     player.position.x = player.position.x + player.velocity.x
-
+    
     local xoffset = 0
     if player.velocity.x > 0 then
         xoffset = 7
     end
-
+    
     local flag = Flag((player.position.x + xoffset) / 8, (player.position.y + 7) / 8)
-
+    
     if flag == 0 then
         player.position.x = startx
     end
-
+    
     player.velocity.y = player.velocity.y + gameRules.gravity
     player.position.y = player.position.y + player.velocity.y
     player.onGround = false
-
+    
     if player.velocity.y >= 0 then
         local flag = Flag((player.position.x + 4) / 8, (player.position.y + 8) / 8)
         if flag == flags.solid then
@@ -102,7 +122,7 @@ function Update(timeDelta)
             player.onGround = true
         end
     end
-
+    
     if player.velocity.y <= 0 then
         local flag = Flag((player.position.x + 4) / 8,(player.position.y ) / 8)
         if flag == flags.solid then
@@ -110,9 +130,51 @@ function Update(timeDelta)
             player.velocity.y = 0
         end
     end
+    
+    -- Plant pot collision y
+    if math.abs(player.position.y - plantPot.position.y) < 4 and math.abs(player.position.x - plantPot.position.x) < 8 and not plantPot.pickedUp then
+        player.position.y = plantPot.position.y - 4
+        player.velocity.y = 0
+        player.onGround = true
+    end
+
+    -- Pick up pot
+    if Button(Buttons.Up, InputState.Down) and math.abs(player.position.x + player.velocity.x - plantPot.position.x) < 10 and not upHeldDown then
+        -- Put down pot
+        if plantPot.pickedUp then
+            plantPot.position.y = player.position.y
+        end
+        plantPot.pickedUp = not plantPot.pickedUp
+        upHeldDown = true
+    end
+
+    if Button(Buttons.Up, InputState.Released) then
+        upHeldDown = false
+    end
+
+    -- Update Plant pot position
+    if plantPot.pickedUp then
+        local plantOffset = (player.isLeft and -8 or 8)
+        plantPot.position.x = player.position.x + plantOffset
+        plantPot.position.y = player.position.y - 3
+    end
+
+    -- Change time
+    if Button(Buttons.B, InputState.Down) and not bHeldDown then
+        isFuture = not isFuture
+        local bgColor = (isFuture and 1 or 0)
+        BackgroundColor( bgColor )
+        bHeldDown = true
+    end
+
+    if Button(Buttons.B, InputState.Released) then
+        bHeldDown = false
+    end
+
 end
 
 function Draw()
     RedrawDisplay()
-    DrawSprite ( player.sprite, player.position.x, player.position.y , player.isLeft, false, DrawMode.Sprite, 0)
+    DrawSprite( player.sprite, player.position.x, player.position.y , player.isLeft, false, DrawMode.Sprite, 0)
+    DrawSprite( plantPot.sprite, plantPot.position.x, plantPot.position.y, false, false, DrawMode.Sprite, 0)
 end
